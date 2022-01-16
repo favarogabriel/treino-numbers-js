e6ce9534436cc30cd0796b67dc7ae61fb7858a0d
Gabriel Favaro <89041502+gabrielffav@users.noreply.github.com>
2022-01-16 14:31:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(100 / 0.00000000000001);
+
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+// console.log(Math.max(1,2,5,6,9,3,-500,200,-300));
+// console.log(Math.min(1,2,5,6,9,3,-500,200,-300));
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+
